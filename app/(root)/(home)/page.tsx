e03fd5f87428f86e4267a5bf5d94cbbfff05a767
hamza-ouaddi <hamza0addi@gmail.com>
2024-05-08 23:13:17 +0100

    add hero section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,10 @@
+import Hero from "@/components/Hero";
 import React from "react";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Hero />
+    </main>
+  );
 }
