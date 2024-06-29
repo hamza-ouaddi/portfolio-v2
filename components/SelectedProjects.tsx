09233b8f95943a3ddf05710b746c0ecdaf509cf5
hamza-ouaddi <hamza0addi@gmail.com>
2024-06-29 18:05:43 +0100

    fix skills data and animation

--- components/SelectedProjects.tsx
+++ components/SelectedProjects.tsx
@@ -8,10 +8,10 @@ import { useInView } from "react-intersection-observer";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Projects() {
-  const {ref} = useSectionInView("Projects", 0.4);
-  
+  const { ref } = useSectionInView("Projects", 0.4);
+
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
       <h2 className="text-3xl text-center font-medium capitalize mb-8">
         Projects
       </h2>
