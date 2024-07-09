7a0c22b68727080de1d8946501378884e03eadf0
hamza-ouaddi <hamza0addi@gmail.com>
2024-07-09 23:56:38 +0100

    style the contact form email using react email

--- lib/actions/contact.action.ts
+++ lib/actions/contact.action.ts
@@ -2,12 +2,14 @@
 
 import { Resend } from "resend";
 import { contactFormInputValidation } from "../utils";
+import ContactFormEmail from "@/email/contact-form-email";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(formData: FormData) {
-  const contactEmail = formData.get("contactEmail");
-  const contactMessage = formData.get("contactMessage");
+  const contactEmail = formData.get("contactEmail")  as string;
+  const contactMessage = formData.get("contactMessage")  as string;
 
   if (!contactFormInputValidation(contactEmail, 250)) {
     return {
@@ -28,6 +30,10 @@ export async function sendEmail(formData: FormData) {
       subject: "Hello world",
       reply_to: contactEmail as string,
       text: contactMessage as string,
+      react: React.createElement(ContactFormEmail, {
+        contactEmail,
+        contactMessage,
+      }),
     });
   } catch (error) {
     throw error;
