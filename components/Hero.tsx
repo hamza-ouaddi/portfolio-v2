7075aea9fb449c9ce95ad7afd1a66230adcfdd9d
hamza-ouaddi <hamza0addi@gmail.com>
2024-08-05 23:30:42 +0100

    upgrade hero section user interface

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,9 +15,9 @@ export default function Hero() {
     <section
       ref={ref}
       id="home"
-      className="mb-28 text-center max-w-[55rem] sm:mb-0 scroll-mt-96"
+      className="mb-28 text-center w-[72rem] sm:mb-0 scroll-mt-96"
     >
-      <div className="flex justify-center items-center">
+      <div className="flex justify-start items-center">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -27,49 +27,47 @@ export default function Hero() {
             src={personalImage}
             alt="Hamza Ouaddi's Image"
             priority={true}
-            className="h-24 w-24 rounded-full border-[0.35rem] border-white shadow-xl object-cover"
+            className="h-36 w-36 rounded-full  shadow-xl object-cover"
           />
         </motion.div>
       </div>
 
-      {/* <p>
-        Hello, I'm Hamza Ouaddi, a full-stack developer, and UI/UX designer. I'm
-        all about creating user-friendly solutions that not only work well but
-        also look awesome. Let's team up and turn your concepts into reality!
-      </p> */}
-
       <motion.h1
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-        className="mb-10 mt-4 px-4 text-2xl sm:text-4xl font-medium !leading-[1.5]"
+        className=" mt-4 px-4 text-2xl sm:text-5xl text-left font-bold !leading-[1.5]"
       >
-        <span className="font-bold"> Hello</span>, I'm{" "}
-        <span className="font-bold">Hamza Ouaddi</span>, a{" "}
-        <span className="font-bold">full-stack developer</span>, and
-        <span className="font-bold"> UI/UX designer.</span> I'm passionate about
-        creating <span className="font-bold">user-friendly</span> and{" "}
-        <span className="font-bold">visually appealing </span> digital
-        solutions.
+        <span className="font-bold"> Hello, I'm Hamza Ouaddi</span>
       </motion.h1>
+
+      <motion.h2
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+        className="mb-10 mt-2 px-4 text-secondaryColor text-2xl sm:text-4xl text-left font-semibold !leading-[1.2]"
+      >
+        A Full Stack Developer and UI/UX Designer <br /> who is passionate about
+        creating user-friendly <br /> and visually appealing digital solutions.
+      </motion.h2>
 
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.1 }}
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium "
+        transition={{ delay: 0.2 }}
+        className="flex flex-col sm:flex-row items-center justify-start gap-2 px-4 text-lg font-medium "
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
-          focus:scale-110  active:scale-105 hover:scale-110 hover:bg-gray-950 transition"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center justify-center gap-2 rounded-[10px] outline-none 
+          focus:scale-110 hover:scale-105 hover:bg-gray-950 transition"
         >
           Get in touch{" "}
           <BsArrowRight className="group-hover:translate-x-1 transition opacity-70" />
         </Link>
         <Link
           href="/resume"
-          className="group bg-white dark:bg-white/10 px-7 py-3 flex items-center gap-2 rounded-full outline-none 
-          focus:scale-110 active:scale-105 hover:scale-110 transition border border-black/10"
+          className="group bg-white dark:bg-white/10 px-7 py-3 flex items-center gap-2 rounded-[10px] outline-none 
+          focus:scale-110 hover:scale-105 transition border border-black/10"
         >
           My resume{" "}
           <BsArrowRight className="group-hover:translate-x-1 transition opacity-70" />
@@ -78,7 +76,7 @@ export default function Hero() {
           <Link
             href="https://www.linkedin.com/in/hamza-ouaddi/"
             className="bg-white dark:bg-white/10 p-4 text-gray-700 dark:text-white/60 flex items-center gap-2 rounded-full outline-none 
-          focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 transition border border-black/10"
+          focus:scale-[1.15] hover:scale-[1.10]  hover:text-gray-950 transition border border-black/10"
           >
             <BsLinkedin />
           </Link>
@@ -86,14 +84,14 @@ export default function Hero() {
             href="https://github.com/hamza-ouaddi"
             target="_blank"
             className="bg-white dark:bg-white/10 p-4 text-gray-700 dark:text-white/60 flex items-center gap-2 rounded-full outline-none 
-          focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 transition border border-black/10"
+          focus:scale-[1.15] hover:scale-[1.10] hover:text-gray-950 transition border border-black/10"
           >
             <BsGithub />
           </Link>
           <Link
             href="https://dribbble.com/hamza_adi"
             className="bg-white dark:bg-white/10 p-4 text-gray-700 dark:text-white/60 flex items-center gap-2 rounded-full outline-none 
-          focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 transition border border-black/10"
+          focus:scale-[1.15] hover:scale-[1.10] hover:text-gray-950 transition border border-black/10"
           >
             <BsDribbble />
           </Link>
